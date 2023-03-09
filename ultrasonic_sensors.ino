49d9d26c43ca4781969bba138c8c9e66cf10c531
William Branch <92341368+willbranch1@users.noreply.github.com>
2023-03-09 14:45:53 -0500

    Update ultrasonic_sensors.ino

--- ultrasonic_sensors.ino
+++ ultrasonic_sensors.ino
@@ -1,3 +1,11 @@
+/*
+ * Program that checks each ultrasonic sensor every 250 ms and reports back to the main computer if there are any obstacles with accompanying data. 
+ * It also includes additional functionality such as the ability to print out all of the sensors data at once, as well as the ability to plot
+ * obstacles around the robot. 
+ *
+ * Currently, there are only four sensors that are able to be programmed. But, with the change of NUM_SENSORS and the array of structs u_sensors, you
+ * are able to add additional sensors.
+ */
 struct Ultrasonic {
   int id;
   int angle;
@@ -8,67 +16,14 @@ struct Ultrasonic {
 
 #define SOUND_TO_IN 0.0135039
 #define SAFE_DISTANCE 18  // inches
-//#define NUM_SENSORS  6
-//struct Ultrasonic u_sensors[NUM_SENSORS] = {{1,0,11,13,0.0},
-//                                            {2,45,12,14,0.0},
-//                                            {3,90,13,15,0.0},
-//                                            {4,180,14,16,0.0},
-//                                            {5,270,15,17,0.0},
-//                                            {6,315,16,18,0.0}};
-
 #define NUM_SENSORS 2
 struct Ultrasonic u_sensors[NUM_SENSORS] = {{1, 0, 22, 21, 0.0},
                                             {2, 180, 17, 16, 0.0}};
 
-void read_sensor(struct Ultrasonic *u) {
-  // put code here
-  // u->last_reading = ...
-  digitalWrite(u->trig_pin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(u->trig_pin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(u->trig_pin, LOW);
-  u->last_reading = pulseIn(u->echo_pin, HIGH) * SOUND_TO_IN / 2;
-  //Serial.printf("%d read %f\n", u->angle, u->last_reading);
-}
-
-void read_all_sensors(void) {
-  for (int i = 0; i < NUM_SENSORS; i++) {
-    read_sensor(&u_sensors[i]);
-    //Serial.printf("%d:%f\n", u_sensors[i].angle, u_sensors[i].last_reading);
-  }
-}
-
-void print_sensor_data(void) {
-  char buffer[30];
-  for (int i = 0; i < NUM_SENSORS; i++) {
-    sprintf(buffer, "%d:%s", u_sensors[i].angle, u_sensors[i].last_reading);
-    Serial.println(buffer);
-  }
-}
-
-void graph_sensor_data() {
-  int x, y;
-  for (int i = 0; i < NUM_SENSORS; i++) {
-    x = u_sensors[i].last_reading * cos(u_sensors[i].angle);
-    y = u_sensors[i].last_reading * sin(u_sensors[i].angle);
-    //scale, color, then graph (x,y)
-  }
-}
-
+//scans for obstacles from the given angle range
+//the angle range is set up in four categories: front(315 to 45), right(45 to 135), rear(135 to 225), and left(225 to 315).
+//does not allow for 
 int scan_for_obstacles(int min_angle, int max_angle) {
-
-//  int angle = -1;
-//  int mmin_angle;
-//  for (int i = 0; i < NUM_SENSORS; i++) {
-//    if ((u_sensors[i].angle >= min_angle) && (u_sensors[i].angle <= max_angle)) {
-//      if (u_sensors[i].last_reading <= SAFE_DISTANCE) {
-//        angle = u_sensors[i].angle;
-//        break;
-//      }
-//    }
-//  }
-//  return angle;
   int angle = -1;
   for (int i = 0; i < NUM_SENSORS; i++) {
     if (min_angle < max_angle) {
@@ -90,30 +45,53 @@ int scan_for_obstacles(int min_angle, int max_angle) {
   return angle;
 }
 
+
+//check if the obstacle is in the front
 int check_front_obstacle(void) {
   int angle;
   angle = scan_for_obstacles(315, 45);
   return (angle);
 }
 
+//check if obstacle is to the rear
 int check_rear_obstacle(void) {
   int angle;
   angle = scan_for_obstacles(135, 225);
   return (angle);
 }
 
+//checks if obstacle is to the right
 int check_right_obstacle(void) {
   int angle;
   angle = scan_for_obstacles(45, 135);
   return (angle);
 }
 
+//checks if obstacle is to the left
 int check_left_obstacle(void) {
   int angle;
   angle = scan_for_obstacles(225, 315);
   return (angle);
 }
 
+//calls read_sensor for every available sensor
+void read_all_sensors(void) {
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    read_sensor(&u_sensors[i]);
+  }
+}
+
+//checks an individual, specified sensor
+void read_sensor(struct Ultrasonic *u) {
+  digitalWrite(u->trig_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(u->trig_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(u->trig_pin, LOW);
+  u->last_reading = pulseIn(u->echo_pin, HIGH) * SOUND_TO_IN / 2;
+}
+
+//method that can be used to print the sensor data for each sensor line by line
 void print_sensor_data(int angle) {
   read_all_sensors();
   if (angle < 0) {
@@ -128,6 +106,16 @@ void print_sensor_data(int angle) {
   }
 }
 
+//function that can be used to display a plot of the identified objects around the robot
+void graph_sensor_data() {
+  int x, y;
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    x = u_sensors[i].last_reading * cos(u_sensors[i].angle);
+    y = u_sensors[i].last_reading * sin(u_sensors[i].angle);
+    //scale, color, then graph (x,y)
+  }
+}
+
 void setup() {
   Serial.begin(115200); // open the serial port at 9600 bps:
 
@@ -135,14 +123,11 @@ void setup() {
     pinMode(u_sensors[i].trig_pin, OUTPUT);
     pinMode(u_sensors[i].echo_pin, INPUT);
   }
-  
-  read_all_sensors();
 
 }
 
 void loop() {
   delay(250); // every 250 msec, read all sensors
-  read_all_sensors();
   if (Serial.available()) {
     char com = Serial.read();
     if (com == 'y') {
